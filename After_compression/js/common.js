d343b4cfbe0aeccaea1c45cb8315be1b267c22bd
goha <gohavorsin@gmail.com>
2018-04-13 20:33:22 +1000

    finish

---
+++ After_compression/js/common.js
@@ -0,0 +1,1 @@
+(function($){ $(function(){ $('.menu__icon').on('click',function(){ $('.menu__icon').toggleClass('menu_state_open'); $('.menu_mobile').toggleClass('menu_open'); }); }); $(function(){ $('.link_mobile').on('click',function(){ $('.menu__icon').removeClass('menu_state_open'); $('.menu_mobile').removeClass('menu_open'); }); }); })(jQuery);
